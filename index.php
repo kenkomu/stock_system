9eef748e025406ab17bb4dd87119681bff649041
kenkomu <komukenneth216@gmail.com>
2023-01-16 08:26:40 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'php_action/db_connect.php';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stock Management System</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+    <!-- bootstrap theme -->
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap-theme.min.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/fontawesome.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" type="text/css" href="custom/css/custom.css">
+    <!-- jquery -->
+    <script type="text/javascript" src="assets/jquery/jquery.min.js"></script>
+    <!-- jqueryui -->
+    <link rel="stylesheet" type="text/css" href="assets/jquery-ui/jquery-ui.min.js"></script>
+    <!-- bootstrap js -->
+    <script type="stylesheet" type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                Testing
+            </div>
+            <!-- col-md-5 -->
+        </div>
+        <!--/row -->
+</div>
+<!--/container-->
+</body>
+</html>
